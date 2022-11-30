58b701548a53a180764a0ffc25d02de1aa918d8e
security-airwallex <security-airwallex@airwallex.com>
2022-11-30 14:49:05 +1100

    additional dedup query fix

--- pipeline/common/dedup.py
+++ pipeline/common/dedup.py
@@ -1,18 +1,22 @@
 """ Fetch event deduplication """
 
-from secops_common.bigquery import latest_rows, get_table, insert_rows
+from secops_common.bigquery import get_table, insert_rows, run_query
 from secops_common.functional import merge, compose2, flatten
 
 from google.cloud import bigquery
 
+from pipeline.common.config import CONFIG
+
 def get_id(row):
     return row['id']
+
+project = CONFIG['project']
 
 def new_events(events, fn_id, table):
   get_table(table)
   ids = set(map(fn_id, events))
-  query = f"SELECT id FROM $table WHERE ids IN @ids;"
-  # ID's are provided externally, we prevent injection
+  query = f"SELECT id FROM `{project}.{table}` WHERE ids IN (@ids);"
+  # ID's are provided externally, we prevent injection by using a prep statement
   job_config = bigquery.QueryJobConfig(
     query_parameters=[
         bigquery.ArrayQueryParameter("ids", "STRING", ids),
