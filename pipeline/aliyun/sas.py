9ca95548e30e1179879820735312df32b35775e4
security-airwallex <security-airwallex@airwallex.com>
2022-12-15 14:57:18 +1100

    removing prints

--- pipeline/aliyun/sas.py
+++ pipeline/aliyun/sas.py
@@ -74,8 +74,6 @@ def _get_alarms(start, end):
     request = DescribeAlarmEventListRequest.DescribeAlarmEventListRequest()
     request.set_From('sas')
     date_format = '%Y-%m-%d %H:%M:%S'
-    print(start.strftime(date_format))
-    print(end.strftime(date_format))
     request.set_TimeStart(start.strftime(date_format))
     request.set_TimeEnd(end.strftime(date_format))
     request.set_CurrentPage(0)
